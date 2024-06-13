47ce49cf403166327403d5e567297de12fed1f7c
Likeapro <iamlikeaproforyou@gmail.com>
2024-06-13 22:57:06 +0530

    Created a modal and setup the modal for creating tags

--- client/src/components/Modal/Modal.component.tsx
+++ client/src/components/Modal/Modal.component.tsx
@@ -1,6 +1,22 @@
-const Modal = () => {
+import './Modal.styles.scss'
+import { ModalContext } from '../../context/modal.context'
+import { ReactNode, useContext } from 'react'
+
+const Modal = ({children} : {children: ReactNode}) => {
+  const {modal , setModal} = useContext(ModalContext)
+  const toggleModal = () => {
+    setModal(!modal)
+  }
   return (
-    <div>Modal</div>
+    <div className='modal'>
+      <div>
+        <div className="close-btn">
+          <button onClick={toggleModal}>close</button>
+        </div>
+        
+        {children}
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Setting/Setting.component.tsx
+++ client/src/components/Setting/Setting.component.tsx
@@ -1,44 +1,62 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { Layout } from "../Layout/Layout"
 import axios from "axios"
 
+import { ModalContext } from "../../context/modal.context"
+
 import './Setting.styles.scss'
+import Modal from "../Modal/Modal.component"
 interface SettingProps {
     setting: Layout | undefined
 }
 
 const Setting: React.FC<SettingProps> = ({ setting }) => {
-    const [newtag , setNewTag] = useState<string>('')
-    const handleOnChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const { modal, setModal } = useContext(ModalContext);
+
+    const [newtag, setNewTag] = useState<string>('')
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNewTag(e.target.value)
     }
     const uploadTag = () => {
         const newArray = setting?.tags
         newArray?.push(newtag)
-        axios.post('/api/layout' , {
-            ...setting , tags: newArray
+        axios.post('/api/layout', {
+            ...setting, tags: newArray
         })
         setNewTag('')
+        setModal(!modal)
+    }
+    const toggleModal = () => {
+        setModal(!modal)
     }
     return (
-        <div className="setting">
-            <div className="left">
-                <ul>
-                    <li>Maths</li>
-                    <li>Chemistry</li>
-                </ul>
-                <button>Create Tag</button>
-            </div>
-            <div className="right">
-                <ul>
-                    {setting?.tags.map((tagname) => (<li>{tagname}</li>))}
-                </ul>
-                <div>
-                    <input type="text" value={newtag} onChange={(e) => handleOnChange(e)}/>
-                    <button onClick={uploadTag}>+ Create Tag</button>
+        <>
+            {modal &&
+                <Modal>
+                    <div className="tag-modal">
+                        <h1>Create Tag</h1>
+                        <input type="text" value={newtag} onChange={(e) => handleOnChange(e)} />
+                        <button onClick={uploadTag}>+ Create Tag</button>
+                    </div>
+                </Modal>}
+
+            <div className="setting">
+                <div className="left">
+                    <ul>
+                        <li>Maths</li>
+                        <li>Chemistry</li>
+                    </ul>
+                    <button>Create Tag</button>
+                </div>
+                <div className="right">
+                    <ul>
+                        {setting?.tags.map((tagname) => (<li>{tagname}</li>))}
+                    </ul>
+                    <button onClick={toggleModal}>+ Create Tag</button>
                 </div>
             </div>
-        </div>
+        </>
+
     )
 }
 
